02d91e1d32c6411c5ce46a64ffcd855d90c5f6e2
Jean-Claude AZIAHA <aziaha.formations@gmail.com>
2025-06-09 16:31:12 +0200

    add: Affichage des articles, des tags et des catégories sur la page du blog

--- src/Controller/Visitor/Blog/BlogController.php
+++ src/Controller/Visitor/Blog/BlogController.php
@@ -2,15 +2,33 @@
 
 namespace App\Controller\Visitor\Blog;
 
+use App\Repository\CategoryRepository;
+use App\Repository\PostRepository;
+use App\Repository\TagRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class BlogController extends AbstractController
 {
+    public function __construct(
+        private CategoryRepository $categoryRepository,
+        private TagRepository $tagRepository,
+        private PostRepository $postRepository,
+    ) {
+    }
+
     #[Route('/blog', name: 'app_visitor_blog_index', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('pages/visitor/blog/index.html.twig');
+        $categories = $this->categoryRepository->findAll();
+        $tags = $this->tagRepository->findAll();
+        $posts = $this->postRepository->findBy(['isPublished' => true], ['publishedAt' => 'DESC']);
+
+        return $this->render('pages/visitor/blog/index.html.twig', [
+            'categories' => $categories,
+            'tags' => $tags,
+            'posts' => $posts,
+        ]);
     }
 }
